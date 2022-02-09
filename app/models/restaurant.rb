32bedaa67b6ed02b7536845b1a6faf26f143d918
Stefan Koch <webokoch@gmx.de>
2022-02-09 07:53:38 +0100

    added validations for restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :cuisine, presence: true
+  validates :location, presence: true
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -6,22 +6,33 @@ RSpec.describe Restaurant, type: :model do
                         cuisine: 'Korean',
                         location: 'Mitte')
   }
-  it 'should be valid with valid atrributes' do
-    except(subject).to be_valid
+
+  it 'should be valid with valid attributes' do
+    expect(subject).to be_valid
   end
 
   it 'should not be valid without a name' do
     subject.name = nil
-    except(subject).to_not be_valid
+    expect(subject).to_not be_valid
   end
-
+  
   it 'should not be valid without a cuisine' do
     subject.cuisine = nil 
-    except(subject).to_not be_valid
+    expect(subject).to_not be_valid
   end
-
+  
   it 'should not be valid without a location' do
     subject.location = nil
-    except(subject).to_not be_valid
+    expect(subject).to_not be_valid
+  end
+
+  context 'name uniquness' do
+    before { described_class.create!( name: 'YumYum',
+                                      cuisine: 'Chinese',
+                                      location: 'Mitte') 
+    }
+    it 'should not be valid with same name' do
+      expect(subject).to_not be_valid
+    end
   end
 end
