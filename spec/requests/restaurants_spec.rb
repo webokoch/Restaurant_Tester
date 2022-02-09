923194ffb29eaea5b20cab7f4312060465d98120
Stefan Koch <webokoch@gmx.de>
2022-02-09 17:51:59 +0100

    added first controller test for restaurant#index

--- spec/requests/restaurants_spec.rb
+++ spec/requests/restaurants_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Restaurants", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    
+    before { get restaurants_path }
+
+    it 'requests restaurants#index' do
+      expect(assigns(:restaurants)).to eq(Restaurants.all)
+    end
+    
+    it 'renders index template' do
+      expect(response).to render_template('index')
+    end
   end
 end
