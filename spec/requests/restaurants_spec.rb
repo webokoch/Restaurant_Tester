cc133fcefdbbada95fc3ef0ffa0490a1c5b9c86a
Stefan Koch <webokoch@gmx.de>
2022-02-14 20:30:40 +0100

    added tests and action for show, new & create

--- spec/requests/restaurants_spec.rb
+++ spec/requests/restaurants_spec.rb
@@ -1,8 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "Restaurants", type: :request do
+RSpec.describe 'Restaurants', type: :request do
+  let(:valid_attributes) do
+    {
+      name: 'YumYum',
+      cuisine: 'Korean',
+      location: 'Mitte'
+    }
+  end
 
-  describe "GET /index" do
+  let(:invalid_attributes) do
+    {
+      name: '',
+      cuisine: 'Korean',
+      location: 'Mitte'
+    }
+  end
+
+  describe 'GET /index' do
     before { get restaurants_path }
 
     it 'requests restaurants#index' do
@@ -13,4 +28,52 @@ RSpec.describe "Restaurants", type: :request do
       expect(response).to render_template('index')
     end
   end
+
+  describe 'GET /show' do
+    it 'should show restaurant' do
+      restaurant = Restaurant.new(valid_attributes)
+      restaurant.save
+      get restaurant_path(restaurant)
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET /new' do
+    it 'should get the restaurants new form' do
+      get new_restaurant_path
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST /create' do
+    context 'with valid attributes' do
+      it 'creates a new restaurant' do
+        expect do
+          restaurant = Restaurant.new(valid_attributes)
+          restaurant.save
+          post restaurants_path, params: {restaurant: valid_attributes}
+        end.to change(Restaurant, :count).by(1)
+      end
+
+      it 'redirects to created restaurant' do
+        post restaurants_path, params: {restaurant: invalid_attributes}
+        expect(response).to be_successful
+      end
+    end
+
+    context 'with invalid atrributes' do
+      it 'does not create a new restaurant' do
+        expect do
+          post restaurants_path, params: {restaurant: invalid_attributes}
+        end.to change(Restaurant, :count).by(0)       
+      end
+
+      it 'redirects to new restaurant view' do
+        post restaurants_path, params: {restaurant: invalid_attributes}
+        expect(response).to be_successful
+      end
+    end
+  end
+
+
 end
