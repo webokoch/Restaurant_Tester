84f605d3d565aa0cb61e2a8531ba000586f32315
Stefan Koch <webokoch@gmx.de>
2022-02-15 16:23:33 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'clearing database'
+
+Restaurant.destroy_all
+
+puts 'generating Restaurants'
+
+r1 = Restaurant.new(
+    name: 'YumYum',
+    cuisine: 'Korean',
+    location: 'Mitte'
+)
+
+r2 = Restaurant.new(
+    name: 'Coffee Table',
+    cuisine: 'American',
+    location: 'Mitte'
+)
+
+r3 = Restaurant.new(
+    name: 'Burger Town',
+    cuisine: 'American',
+    location: 'Kreuzberg'
+)
+
+r4 = Restaurant.new(
+    name: 'Golden Palace',
+    cuisine: 'Chinese',
+    location: 'Mitte'
+)
+
+r5 = Restaurant.new(
+    name: 'Sushi & Ramen',
+    cuisine: 'Japanese',
+    location: 'Kreuzberg'
+)
+
+r6 = Restaurant.new(
+    name: 'Napoli',
+    cuisine: 'Italian',
+    location: 'Friedrichshain'
+)
+
+r7 = Restaurant.new(
+    name: 'Kebap King',
+    cuisine: 'Turkish',
+    location: 'Kreuzberg'
+)
+
+r8 = Restaurant.new(
+    name: 'Babylon',
+    cuisine: 'Mediterranean',
+    location: 'Friedrichshain'
+)
+
+puts 'Done...'
